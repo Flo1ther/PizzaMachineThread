de70b189bb72b45cba0237d011d3599b219fc776
Flo1ther <shevchuk.nirosla@gmail.com>
2024-10-05 13:18:39 +0300

    Добавьте файлы проекта.

---
+++ PizzaMachineThread/PizzaMachineThread.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMachineThread
+{
+    public class PizzaRobot
+    {
+        public string PizzaName { get; }
+
+        public PizzaRobot(string pizzaName)
+        {
+            PizzaName = pizzaName;
+        }
+
+        public void KneadDough()
+        {
+            Console.WriteLine($"Робот починає замішувати тісто для піци \"{PizzaName}\"...");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Тісто для піци \"{PizzaName}\" готове.");
+        }
+
+        public void AddIngredients()
+        {
+            Console.WriteLine($"Робот додає інгредієнти для піци \"{PizzaName}\"...");
+            Thread.Sleep(3000);
+            Console.WriteLine($"Інгредієнти для піци \"{PizzaName}\" додані.");
+        }
+
+        public void BakePizza()
+        {
+            Console.WriteLine($"Робот випікає піцу \"{PizzaName}\"...");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Піца \"{PizzaName}\" готова.");
+        }
+
+        public void MakePizza()
+        {
+            Thread doughThread = new Thread(KneadDough);
+            Thread ingredientsThread = new Thread(AddIngredients);
+            Thread bakeThread = new Thread(BakePizza);
+
+            doughThread.Start();
+            doughThread.Join();
+
+            ingredientsThread.Start();
+            ingredientsThread.Join();
+
+            bakeThread.Start();
+            bakeThread.Join();
+
+            Console.WriteLine($"Робот завершив приготування піци \"{PizzaName}\"!");
+        }
+    }
+
+}
---
+++ PizzaMachineThread/Program.cs
@@ -0,0 +1,15 @@
+﻿using PizzaMachineThread;
+using System;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+PizzaRobot robot = new PizzaRobot("Маргарита");
+
+Thread pizzaMakingThread = new Thread(robot.MakePizza);
+
+Console.WriteLine("Робот починає процес приготування піци...");
+pizzaMakingThread.Start();
+pizzaMakingThread.Join();
+
+Console.WriteLine("Процес приготування піци завершено!");
